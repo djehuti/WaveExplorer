73c24a56b96211b805ddec206c658a123ce05362
Ben Cox <cox@djehuti.com>
2012-07-17 22:06:44 -0400

    Add WaveTools framework

---
+++ WaveTools.framework/Versions/A/Headers/WaveTools.h
@@ -0,0 +1,11 @@
+//
+//  WaveTools.h
+//  WaveTools
+//
+//  Created by Ben Cox on 7/17/12.
+//  Copyright 2012 Ben Cox. All rights reserved.
+//
+
+
+#import <WaveTools/WaveToolsBundleUtils.h>
+#import <WaveTools/DWTMath.h>
